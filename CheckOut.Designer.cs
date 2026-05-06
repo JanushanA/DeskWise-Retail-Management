558f70ab8263c4d11f70c555ea438c81db35954e
Janushan Anandakumar <0073033@schoolsnet.act.edu.au>
2026-05-06 11:54:53 +1000

    Implemented Checkout Functionality

--- CheckOut.Designer.cs
+++ CheckOut.Designer.cs
@@ -28,59 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.cbReceipt = new System.Windows.Forms.CheckBox();
-            this.btnPurchase = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.dgvAvailable = new System.Windows.Forms.DataGridView();
+            this.dgvCurrentOrder = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDes = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAvailable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCurrentOrder)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // dgvAvailable
             // 
-            this.groupBox1.Controls.Add(this.cbReceipt);
-            this.groupBox1.Controls.Add(this.btnPurchase);
-            this.groupBox1.Location = new System.Drawing.Point(41, 38);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(560, 376);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.dgvAvailable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAvailable.Location = new System.Drawing.Point(18, 37);
+            this.dgvAvailable.Name = "dgvAvailable";
+            this.dgvAvailable.Size = new System.Drawing.Size(213, 520);
+            this.dgvAvailable.TabIndex = 0;
+            this.dgvAvailable.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dgvAvailable_MouseDown);
             // 
-            // cbReceipt
+            // dgvCurrentOrder
             // 
-            this.cbReceipt.AutoSize = true;
-            this.cbReceipt.Location = new System.Drawing.Point(267, 306);
-            this.cbReceipt.Name = "cbReceipt";
-            this.cbReceipt.Size = new System.Drawing.Size(110, 17);
-            this.cbReceipt.TabIndex = 2;
-            this.cbReceipt.Text = "Generate Receipt";
-            this.cbReceipt.UseVisualStyleBackColor = true;
+            this.dgvCurrentOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCurrentOrder.Location = new System.Drawing.Point(271, 37);
+            this.dgvCurrentOrder.Name = "dgvCurrentOrder";
+            this.dgvCurrentOrder.Size = new System.Drawing.Size(499, 341);
+            this.dgvCurrentOrder.TabIndex = 1;
+            this.dgvCurrentOrder.DragDrop += new System.Windows.Forms.DragEventHandler(this.dgvCurrentOrder_DragDrop);
+            this.dgvCurrentOrder.DragEnter += new System.Windows.Forms.DragEventHandler(this.dgvCurrentOrder_DragEnter);
             // 
-            // btnPurchase
+            // btnAdd
             // 
-            this.btnPurchase.Location = new System.Drawing.Point(169, 302);
-            this.btnPurchase.Name = "btnPurchase";
-            this.btnPurchase.Size = new System.Drawing.Size(75, 23);
-            this.btnPurchase.TabIndex = 0;
-            this.btnPurchase.Text = "Purchase";
-            this.btnPurchase.UseVisualStyleBackColor = true;
+            this.btnAdd.Location = new System.Drawing.Point(270, 407);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add to Order";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(372, 407);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(108, 23);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove from Order";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(501, 407);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(600, 407);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save Order";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(695, 407);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "Load Order";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(286, 489);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(34, 13);
+            this.lblTotal.TabIndex = 7;
+            this.lblTotal.Text = "Total:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(395, 492);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Order Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(569, 489);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Order Description";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(398, 509);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 10;
+            // 
+            // txtDes
+            // 
+            this.txtDes.Location = new System.Drawing.Point(572, 508);
+            this.txtDes.Name = "txtDes";
+            this.txtDes.Size = new System.Drawing.Size(100, 20);
+            this.txtDes.TabIndex = 11;
             // 
             // CheckOut
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtDes);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvCurrentOrder);
+            this.Controls.Add(this.dgvAvailable);
             this.Name = "CheckOut";
-            this.Size = new System.Drawing.Size(684, 448);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.Size = new System.Drawing.Size(802, 596);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAvailable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCurrentOrder)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.CheckBox cbReceipt;
-        private System.Windows.Forms.Button btnPurchase;
+        private System.Windows.Forms.DataGridView dgvAvailable;
+        private System.Windows.Forms.DataGridView dgvCurrentOrder;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDes;
     }
 }
--- CheckOut.cs
+++ CheckOut.cs
@@ -15,6 +15,93 @@ namespace WindowsFormsApp10
         public CheckOut()
         {
             InitializeComponent();
+            SetUpOrderSystem(); 
         }
+
+        private BindingList<Product> _availableProducts;
+        private BindingList<OrderItem> _currentOrderItems = new BindingList<OrderItem>();
+        private string _csvPath = "./shop-product-catalog.csv";
+
+        private void SetUpOrderSystem()
+        {
+            dgvAvailable.ReadOnly = true;
+            dgvAvailable.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvAvailable.MultiSelect = false;
+
+            dgvCurrentOrder.ReadOnly = true;
+            dgvCurrentOrder.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvCurrentOrder.MultiSelect = false;
+
+            dgvAvailable.AutoGenerateColumns = false;
+            dgvAvailable.Columns.Clear();
+            dgvAvailable.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "ProductID", HeaderText = "ID", Name = "colID", Width = 50 });
+            dgvAvailable.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "ProductName", HeaderText = "Product Name", Name = "colName", AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill });
+
+            dgvAvailable.AutoGenerateColumns = false;
+            dgvAvailable.Columns.Clear();
+            dgvAvailable.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "ProductID", HeaderText = "ID", Name = "colID", Width = 50 });
+            dgvAvailable.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "ProductName", HeaderText = "Product Name", Name = "colName", AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill });
+
+            dgvCurrentOrder.DataSource = _currentOrderItems;
+            dgvCurrentOrder.AllowDrop = true;
+        }
+
+        private void dgvAvailable_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (dgvAvailable.SelectedRows.Count > 0)
+            {
+                var product = (Product)dgvAvailable.SelectedRows[0].DataBoundItem;
+                dgvAvailable.DoDragDrop(product, DragDropEffects.Copy);
+            }
+        }
+
+        private void dgvCurrentOrder_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(Product)))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void dgvCurrentOrder_DragDrop(object sender, DragEventArgs e)
+        {
+            Product droppedProduct = (Product)e.Data.GetData(typeof(Product));
+            AddProductToOrder(droppedProduct);
+        }
+
+        private void AddProductToOrder(Product product)
+        {
+            var existingItem = _currentOrderItems.FirstOrDefault(i => i.ProductID == product.ProductID);
+            
+            if (existingItem != null)
+            {
+                existingItem.Quantity++;
+            }
+            else
+            {
+               
+                _currentOrderItems.Add(new OrderItem
+                {
+                    ProductID = product.ProductID,
+                    ProductName = product.ProductName,
+                    UnitPrice = product.ProductPrice,
+                    Quantity = 1
+                });
+            }
+           
+            _currentOrderItems.ResetBindings();
+
+            UpdateTotal();
+        }
+
+
+        private void UpdateTotal()
+        {
+            
+            decimal total = _currentOrderItems.Sum(i => i.Subtotal);
+            lblTotal.Text = $"Total: ${total:F2}";
+        }
+
+
+
     }
+
 }
